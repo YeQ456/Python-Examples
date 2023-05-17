ac3bf0b2b7eece50d2f50801a9e73032ae750637
HatsuneMiku~39 <57651561+18096076730@users.noreply.github.com>
2023-05-17 14:09:03 +0800

    例1.反转一个3位整数.py

---
+++ 入门/例1.反转一个3位整数.py
@@ -0,0 +1,29 @@
+# 问题描述
+# 反转一个只有3位的整数
+
+# 示例
+# 输入：123
+# 输出：321
+
+# 输入：900
+# 输出：9
+
+# 源码实现
+class Solution:
+    def reverseInteger(self, number):
+        a = int(number / 100)            # 百位数
+        b = int(number % 100 / 10)       # 十位数
+        c = int(number % 10)             # 个位数
+        return(100 * c + 10 * b + a)     # 反转：个位数*100 + 十位数*10 + 百位数
+    
+if __name__ == '__main__':
+    solution = Solution()
+    num = 123
+    ans = solution.reverseInteger(num)
+    print("Input: ", num)
+    print("Output: ", ans)
+
+
+# 运行结果
+# Input: 123
+# Output: 321
